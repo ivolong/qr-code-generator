f4281f99553f2a442d03c4c8a5c5ee17dd6a1433
ivo <ivolong@gmail.com>
2024-05-15 18:14:29 -0400

    make visually more appealing

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const qrCodeOutput = document.getElementById("qr-code-output");
 
 const qrCode = new QRCode(qrCodeOutput, {
-    height: 768,
-    width: 768,
+    height: 750,
+    width: 750,
 });
 
 
