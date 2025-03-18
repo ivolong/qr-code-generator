3850e3d0b4fc14087d4a7d281aad378dfaa9da01
ivo <36451843+ivolong@users.noreply.github.com>
2025-03-18 10:46:51 -0400

    fix: remove debounce

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
 const qrCodeOutput = document.getElementById("qr-code-output");
 
-const debounce = (callback, wait) => {
-    let timeoutId = null;
-
-    return (...args) => {
-        window.clearTimeout(timeoutId);
-
-        timeoutId = window.setTimeout(() => {
-            callback.apply(null, args);
-        }, wait);
-    };
-};
-
 const qrCode = new QRCode(qrCodeOutput, {
     height: 750,
     width: 750,
